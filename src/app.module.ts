f23c168494654a2e0f94cadc6afd2662774295c5
luchian_neos <luchian.bordiyan@neosperience.com>
2025-11-27 14:54:57 +0100

    feat: wip - setup users feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './features/users/user.entity';
+import { UsersModule } from './features/users/users.module';
 
 @Module({
   imports: [
@@ -14,9 +16,10 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
